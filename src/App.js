35fcaac185c2ce1cb93aa366f74ed4f3940d8a9a
theweicode <theweicode@gmail.com>
2019-05-14 21:41:57 -0700

    test branch 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is the master branch</p>
+        <p>This is the branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
